e8ab425736355ae69631ca235814e8e6688fe9b5
Ramahadi Jan <moheanerj@gmail.com>
2021-09-08 11:11:42 +0200

    imported and configured handlebars module and created a folder for views

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 let express = require('express');
+const exphbs = require('express-handlebars');
+
+//create instance of an App
 let app = express();
 
+//configure express-handlebars
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars')
+
+//routing
 app.get('/', function (req, res) {
     res.send('Bill Settings WebApp ');
 });
 
+//setting port
 let PORT = process.env.PORT || 3007;
 
+
+//listening to the server
 app.listen(PORT, function () {
     console.log('App starting on port', PORT);
     
